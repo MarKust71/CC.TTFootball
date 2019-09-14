45bae04b6d3f40a651496e1370b220fb97cd1517
MareK <markust71@hotmail.com>
2019-09-14 08:19:02 +0200

    .env again...

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -2,10 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    if (process.env.PORT) {
-        res.send(`Hello, players! Welcome on port ${process.env.PORT}!`);
+    if (process.env.TEST_ENV) {
+        if (process.env.PORT) {
+            res.send(`Hello, players! Welcome on port ${process.env.PORT}! (${process.env.TEST_ENV})`);
+        } else {
+            res.send(`Hello, players! Welcome on board! (${process.env.TEST_ENV})`);
+        }
     } else {
-        res.send(`Hello, players! Welcome on board!`);
+        if (process.env.PORT) {
+            res.send(`Hello, players! Welcome on port ${process.env.PORT}!`);
+        } else {
+            res.send(`Hello, players! Welcome on board!`);
+        }
     }
 })
 
