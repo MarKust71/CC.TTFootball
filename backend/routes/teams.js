a087815bde1ca06600f1c003f30606c3cd29fb4c
MareK <markust71@hotmail.com>
2019-09-17 23:17:40 +0200

    backup

--- backend/routes/teams.js
+++ backend/routes/teams.js
@@ -7,16 +7,22 @@ const Region = require('./mk_regions');
 
 
 const teamSchema = new mongoose.Schema({
+    id: { type: Number, required: true, unique: true },
     name: { type: String, required: true, unique: true },
     playersId: [ { type: mongoose.Schema.Types.ObjectId,  ref: 'Player'} ],
-    regionId: { type: mongoose.Schema.Types.ObjectId,  ref: 'Region'}
+    regionId: { type: mongoose.Schema.Types.ObjectId,  ref: 'Region'},
+    isDeleted: { type: Boolean }
 });
 const Team = mongoose.model('Team', teamSchema);
 async function createTeam(name, players, region) {
     const team = new Team({
+        id: await getMaxTeamId()
+        .then( (res) => { return res + 1; } )
+        .catch( (err) => { console.log('Something went wrong (new getMax):', err.errmsg); return -1 } ),
         name: name,
         playersId: players,
-        regionId: region
+        regionId: region,
+        isDeleted: false
     });
     return await team.save()
         .then( (res) => { 
@@ -28,18 +34,34 @@ async function createTeam(name, players, region) {
             return err.errmsg;
         } );
 }
-// createTeam('Team1', ['5d7e7d992da6aa4260a83d23', '5d7e7d992da6aa4260a83d24'], '5d7e7d992da6aa4260a83d22');
+// createTeam('Team3', ['5d7e7d992da6aa4260a83d23', '5d7e7d992da6aa4260a83d24'], '5d7e7d992da6aa4260a83d22');
 
-async function getTeams() {
-    const result = await Team
+async function getMaxTeamId() {
+    return await Team
         .find()
-        .populate('regionId', 'name id -_id')
-        .populate('playersId')
-        // .then( res => console.log(res) )
-        // .then( res => { return res } )
-        .catch( err => console.log('Something went wrong...', err));
-    // console.log(result);
-    return result;
+        .sort( {id : -1} )
+        .limit(1)
+        .then( (res) => { return res[0].id } )
+        .catch( (err) => { console.log('Something went wrong (getMax):', err.message); return -1; } );
+}
+
+async function getTeams(id) {
+    if (id) {
+        return await Team
+            .find( { id: id } )
+            .populate('regionId', 'name id -_id')
+            .populate('playersId')
+            // .then( result => { console.log(result[0]); } )
+            .then( result => { return result[0]; } )
+            .catch( err => console.log('Something went wrong...', err));
+    } else {
+        // const result = await Team
+        return await Team
+            .find()
+            .populate('regionId', 'name id -_id')
+            .populate('playersId')
+            .catch( err => console.log('Something went wrong...', err));
+    }
 }
 // getTeams();
 
@@ -47,6 +69,27 @@ async function getTeams() {
 
 router.get('/', (req, res) => {
     getTeams().then( result => { res.send(result) } )
+})
+
+router.get('/:id', (req, res) => {
+    // getTeams(req.params.id)
+    //     .then( result => { res.send(result) } );
+
+    getTeams(req.params.id)
+        .then( result => { 
+            if (!result) res.status(404).send(`Team ID: ${req.params.id} not found`); 
+            res.send(result); 
+        } );
+
+        // const id = req.params.id;
+    // const team = await getTeams(id).then( result => { return result; } );
+    // if (!team) res.status(404).send(`Team ID: ${id} not found`);
+    // res.send(team);
+})
+
+router.put('/:id', (req, res) => {
+    console.log('put->', req.params.id);
+    res.send(`put-> ${req.params.id}`);
 })
 
 router.post('/', (req, res) => {
