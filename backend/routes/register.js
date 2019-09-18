0e86cb019303cfc7df512b7678a041685e096ef6
Piotr Bocian <piotr.bocian.96@gmail.com>
2019-09-18 21:46:59 +0200

    register v2

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -10,13 +10,13 @@ router.post('/', async(req, res)=> {
     
     if (error) return res.status(400).send(error.details[0].message);
 
-    let userEmail = await User.findOne({ email: req.body.email})
+    const userEmail = await User.findOne({ email: req.body.email})
     if (userEmail) return res.status(400).send('Email zajęty!');
     
-    userLogin = await User.findOne({ login: req.body.nickname})
+    const userLogin = await User.findOne({ login: req.body.nickname})
     if (userLogin) return res.status(400).send('Login zajęty!');
 
-    user = new User(_.pick(req.body, ['nickname','email','password','name','surname']));
+    const user = new User(_.pick(req.body, ['nickname','email','password','name','surname']));
 
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt); 
@@ -27,7 +27,7 @@ router.post('/', async(req, res)=> {
 
 function validate(req) {
     const schema = {
-      nickname: Joi.string().min(5).max(30).required(), //dłuższy login?
+      nickname: Joi.string().min(5).max(30).required(), 
       email: Joi.string().min(5).max(255).required().email(),
       password: Joi.string().min(5).max(1024).required(),
       name: Joi.string().max(30),
