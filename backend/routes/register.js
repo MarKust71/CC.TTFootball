c2c3cf60d2df387e2b0d5926465990bc11a6795b
CaptainObjective <marcinwos18@gmail.com>
2019-09-26 03:21:29 +0200

    chyba tak mialo byc

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -5,29 +5,6 @@ const _ = require('lodash');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-  function validate(req) {
-    const schema = {
-      nickname: Joi.string()
-        .min(5)
-        .max(30)
-        .required(),
-      email: Joi.string()
-        .min(5)
-        .max(255)
-        .required()
-        .email(),
-      password: Joi.string()
-        .min(5)
-        .max(1024)
-        .required(),
-      name: Joi.string().max(30),
-      surname: Joi.string().max(30),
-      division: Joi.string().required(),
-    };
-
-    return Joi.validate(req, schema);
-  }
-
   const { User, Division } = res.locals.models;
   const { error, value } = validate(req.body);
   if (error) return res.status(400).json(error.details);
