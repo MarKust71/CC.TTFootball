76be515079ef4e7c55cf172a0a5bd2e36643669b
MareK <markust71@hotmail.com>
2019-09-28 08:37:46 +0200

    install+update

---
+++ frontend/src/components/Teams/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { Button, Form, Segment } from 'semantic-ui-react';
+
+class Teams extends React.Component {
+    
+}
