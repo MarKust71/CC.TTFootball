4d50d963d451a94b796a31c234ba7f3b80a13321
Unknown <marcinwos18@gmail.com>
2019-09-30 16:19:17 +0200

    Propozycje Nawigacji dla Marcina

--- frontend/src/components/AppBar/index.jsx
+++ frontend/src/components/AppBar/index.jsx
@@ -1,7 +1,11 @@
-import React, { useContext, Component } from 'react';
-import { Link } from 'react-router-dom';
-import {Menu, Segment} from 'semantic-ui-react';
+import React, { useContext } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { Menu, Segment } from 'semantic-ui-react';
 import Store from '../../Store';
+
+const MyLink = () => {
+  return <NavLink></NavLink>;
+};
 
 const AppBar = () => {
   const { isLogged, changeStore } = useContext(Store);
@@ -15,21 +19,20 @@ const AppBar = () => {
       <div className="center">
         <img alt="nie ma obrazka" src="images/nowelogo.jpg" />
       </div>
-        <Menu secondary>
-          <Menu.Item as={Link} name='PANkarzyki' to='/' />
-            
-          {isLogged && (
-            <Menu.Menu>
-              <Menu.Item as={Link} name='Główna' to='/'/>            
-              <Menu.Item as={Link} name='Terminarz' to='/Schedule'/>    
-              <Menu.Item as={Link} name='Ligi' to='/Leagues'/>     
-              <Menu.Item as={Link} name='Drużyny' to='/Teams' />     
-              <Menu.Item as={Link} name='Nowa Liga' to='/NewLeague'/>  
-            </Menu.Menu>  
-                                    
-          )}        
-          <Menu.Item as={Link} name='Wyjdź' to='/' onClick={handleLogout} position='right' />
-        </Menu>
+      <Menu secondary>
+        <Menu.Item as={Link} name="PANkarzyki" to="/" />
+
+        {isLogged && (
+          <Menu.Menu>
+            <Menu.Item as={NavLink} name="Główna" to="/" activeClassName="active" exact />
+            <Menu.Item as={NavLink} name="Terminarz" to="/Schedule" activeClassName="active" />
+            <Menu.Item as={NavLink} name="Ligi" to="/Leagues" activeClassName="active" />
+            <Menu.Item as={NavLink} name="Drużyny" to="/Teams" activeClassName="active" />
+            <Menu.Item as={NavLink} name="Nowa Liga" to="/NewLeague" activeClassName="active" />
+          </Menu.Menu>
+        )}
+        <Menu.Item as={Link} name="Wyjdź" to="/" onClick={handleLogout} position="right" />
+      </Menu>
     </div>
   );
 };
