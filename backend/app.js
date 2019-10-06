7c98ef827ddee11959ed863011bf73bc91235949
MareK <markust71@hotmail.com>
2019-10-06 13:45:31 +0200

    update20

--- backend/app.js
+++ backend/app.js
@@ -61,6 +61,7 @@ const main = async () => {
     app.use(express.static(path.join(__dirname, '../frontend/build')));
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
+    console.log(path.join(__dirname, '../frontend/build', 'index.html'));
       res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
     });
   }
