af39190d7e5106186f33c571b4db3eebe83b7d2f
Marcin Sala <toriuke44@gmail.com>
2019-10-06 21:00:51 +0200

    Update

--- frontend/src/Views/Homepage/index.jsx
+++ frontend/src/Views/Homepage/index.jsx
@@ -2,23 +2,7 @@ import React from 'react';
 import { Segment } from 'semantic-ui-react';
 import { TopMenu } from '../../components/Homepage';
 
-// const me = JSON.parse(localStorage.me);
-
-// const Home = () => {
-//   console.log(me);
-//   return (
-//     <div className="container">
-//       <main>
-//         <p>
-//           O co chodzi? Gramy ligę!! Dobierz się w drużynę, zapisz do rozgrywek, zapisuj wyniki, sprawdzaj kto kogo
-//           złoił. Wyłonimy zwycięzcę! :) Wanna Join?
-//         </p>
-//       </main>
-//     </div>
-//   );
-// };
 const Home = () => {
-  // console.log(me);
   return (
     <Segment>
       <TopMenu></TopMenu>
