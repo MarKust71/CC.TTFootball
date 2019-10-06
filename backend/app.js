df7b860e11a45b091c569e569d2095878ab8bd2c
MareK <markust71@hotmail.com>
2019-10-06 12:20:41 +0200

    update14

--- backend/app.js
+++ backend/app.js
@@ -55,6 +55,15 @@ const main = async () => {
 
   app.use(errorHandler);
 
+  if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'frontend/build')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+    });
+  }
+
   // Listening
   const host = process.env.HOST || '127.0.0.1';
   const port = process.env.PORT || 8080;
